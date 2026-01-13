2d8d31b187908f69b7eb2fccfd980e506f206782
Jordan Brockbank <132620463+JBrockbank@users.noreply.github.com>
2026-01-13 12:55:44 -0700

    add(added workflow check for index.html

--- app/projects/cfb/gallery/page.tsx
+++ app/projects/cfb/gallery/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
-// import "yet-another-react-lightbox/styles.css";
 
 const images = [
   { src: "/cfb/home.png", alt: "Home screen" },
